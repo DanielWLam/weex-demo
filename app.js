eb94646f380cd88fe932b2896212e4206c53b124
DanielWLam <lwd931227@126.com>
2017-03-15 18:29:31 +0800

    用stream.fetch来做请求

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-// import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
 import App from './src/App.vue'
-// import foo from './src/foo.vue'
-
-Vue.use(ElementUI)
 App.el = '#root'
-// foo.el = '#root'
 
 export default new Vue(App)
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <div class="wrapper" @click="update">
-    <image :src="logoUrl" class="logo"></image>
-    <text class="title">Hello {{target}}</text>
-  </div>
+  <list class="list">
+    <cell class="cell" v-for="item in lists">
+      <div class="panel">
+        <text class="text">{{item.title}}</text>
+      </div>
+    </cell>
+  </list>
 </template>
 
 <style>
-  .wrapper { align-items: center; margin-top: 120px; }
-  .title { font-size: 48px; }
-  .logo { width: 360px; height: 82px; }
 </style>
 
 <script>
+  var stream = weex.requireModule('stream')
   export default {
     data: {
-      logoUrl: 'https://alibaba.github.io/weex/img/weex_logo_blue@3x.png',
-      target: 'World'
+      lists: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    mounted () {
+      this.getTopic()
     },
     methods: {
-      update: function (e) {
-        this.target = 'Weex'
-        console.log('target:', this.target)
+      getTopic () {
+        // var xhr = new window.XMLHttpRequest()
+        // xhr.open('GET', 'https://cnodejs.org/api/v1/topics', true)
+        // xhr.onreadystatechange = () => {
+        //   if (xhr.readyState === 4) {
+        //     if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+        //       try {
+        //         var tem = JSON.parse(xhr.responseText)
+        //         this.lists = tem.data
+        //         console.log(this.lists)
+        //       } catch (err) {
+        //         console.log(err)
+        //       }
+        //     } else {
+        //       console.log('Request was unsuccessful : ' + xhr.status)
+        //     }
+        //   }
+        // }
+        // xhr.send(null)
+        // fetch('https://cnodejs.org/api/v1/topics')
+        // .then(response => {
+        //   return response.json()
+        // }).then(data => {
+        //   this.lists = data.data
+        // }).catch(err => {
+        //   console.log(err)
+        // })
+        return stream.fetch({
+          method: 'GET',
+          type: 'json',
+          url: 'https://cnodejs.org/api/v1/topics'
+        }, (res) => {
+          this.lists = res.data.data
+        })
       }
     }
   }
